48c08f6f987779a7bbc20dae53992279c249e6e3
Christian Janker <janker.christ@gmail.com>
2019-01-16 20:18:04 +0100

    feat(route-param-hoc): fix bug in commits.container.ts (duplicated dumb component) - reduced rendering of all components to one

--- src/app/components/commitList/commitList.component.ts
+++ src/app/components/commitList/commitList.component.ts
@@ -1,10 +1,11 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, ChangeDetectionStrategy} from '@angular/core';
 import {Commit} from './commit';
 
 @Component({
   selector: 'commit-list',
   templateUrl: './commitList.component.html',
-  styleUrls: ['./commitList.component.scss']
+  styleUrls: ['./commitList.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CommitListComponent implements OnInit {
    @Input()
